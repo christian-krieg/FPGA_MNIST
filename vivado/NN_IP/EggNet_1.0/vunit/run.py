9b3220216ea08095a27dc043873e403938669b08
Benjamin K <benjamin.kulnik@student.tuwien.ac.at>
2020-05-25 19:27:59 +0200

    Pep8 formatting + minor fixes, added --compile-only flag

--- vivado/NN_IP/EggNet_1.0/vunit/run.py
+++ vivado/NN_IP/EggNet_1.0/vunit/run.py
@@ -9,6 +9,8 @@ Created on Mon Mar 23 12:20:15 2020
 @author: lukas
 @author: Benjamin
 """
+
+import importlib.util
 import sys
 
 import os
@@ -27,14 +29,13 @@ import getpass
 ROOT = pathlib.Path(__file__).parent
 SRC_ROOT = pathlib.Path(__file__).parent.parent / 'src'
 
-
-
-
-if sys.platform == "Windows" or "win32":
-    DEFAULT_UNISIM_ROOT = pathlib.WindowsPath("C:/msys64/mingw64/lib/ghdl/vendors/xilinx-vivado/unisim/v08")
+if sys.platform in ["Windows", "win32"]:
+    DEFAULT_UNISIM_ROOT = pathlib.WindowsPath(
+        "C:/msys64/mingw64/lib/ghdl/vendors/xilinx-vivado/unisim/v08")
 else:
     # DEFAULT_UNISIM_ROOT = pathlib.Path("/usr/local/lib/ghdl/vendors/xilinx-vivado/unisim/v08")
-    DEFAULT_UNISIM_ROOT = pathlib.Path("./lib/unisim-debian-2019_2/xilinx-vivado/unisim/v08")
+    DEFAULT_UNISIM_ROOT = pathlib.Path(
+        "./lib/unisim-debian-2019_2/xilinx-vivado/unisim/v08")
     # UNISIM_ROOT_BENNI = pathlib.Path("./lib/lib/unisim-debian-2019_2/xilinx-vivado/unisim/v08")
 
 # ---------------------------
@@ -42,30 +43,40 @@ else:
 # ---------------------------
 
 # Example Usage:
-# 
+#
 #   python run.py -r --testpath ./sim ./sim2 ./sim3 --unisim ./lib/unisim-debian-2019
 parser = argparse.ArgumentParser(
     description="This script invokes the build of the Eggnet VHDL files using VUNIT and GHDL"
 )
-parser.add_argument("--testpath", default="./testbenches", nargs="+", type=str, metavar="testpath", 
+parser.add_argument("--compile-only", action="store_true",
+                    help="Only compile the source files without running any testbenches")
+parser.add_argument("--testpath", default="./testbenches", type=str, metavar="testpath",
                     help="Specify the path that should be scanned for testbenches")
-parser.add_argument("-t", "--testbench", default="all", nargs="+" , type=str, metavar="testbench",
+parser.add_argument("-t", "--testbench", default="all", nargs="+", type=str, metavar="testbench",
                     help="Speify the used testbench. Use \'all\' to use all testbenches in testpath")
-parser.add_argument("-r", "--recursive", default=True, 
+parser.add_argument("-r", "--recursive", default=True,
                     help="Enable recursive test search and execution of the provided test path")
-parser.add_argument("--unisim", default=DEFAULT_UNISIM_ROOT, type=str, metavar="unisim_path", 
+parser.add_argument("--unisim", default=DEFAULT_UNISIM_ROOT, type=str, metavar="unisim_path",
                     help="The path of the compiled unisim package")
 parser.add_argument("-g", "--gui", action="store_true",
                     help="Launch the graphical user interface (coming soon!)")
 parser.add_argument("--vcd", action="store_true", dest='vcd',
                     help="Enable vcd waveform file output")
-parser.add_argument("--synopsys", action="store_true",dest='synopsys',
+parser.add_argument("--synopsys", action="store_true", dest='synopsys',
                     help="Use synopsys library in ghdl")
-args = parser.parse_args()
+parser.add_argument("--unisim-src", action="store_true",
+                    help="Unisim path points to source files, needs to be compiled")
+
+
+# args = parser.parse_args()
+args = parser.parse_args(args=['--compile-only', '--unisim-src',
+                               '--unisim', 'lib/unisim-src', '--testpath', './vunit/testbenches'])
+
 
 if pathlib.Path(args.unisim).exists() == False:
-    #raise Exception("You need to provide a path for pre-compiled `unisim` in VHDL2008 standard to compile the lib (see ghdl doc)")
-    args.unisim = DEFAULT_UNISIM_ROOT
+    raise Exception(
+        "You need to provide a path for pre-compiled `unisim` in VHDL2008 standard to compile the lib (see ghdl doc)")
+
 
 if args.gui == True:
     print("GUI is not ready yet, continuing in command line mode")
@@ -85,10 +96,19 @@ VU.enable_location_preprocessing()
 VU.enable_check_preprocessing()
 VU.add_osvvm()  # Add support for OSVVM
 VU.add_json4vhdl()
-VU.add_external_library("unisim", args.unisim)
+
+# -- Add the Unisim library
+if args.unisim_src == True:
+    # -- If source files are used, add them manually
+    lib_unisim = VU.add_library("unisim", vhdl_standard="08")
+    lib_unisim.add_source_files(pathlib.Path(args.unisim) / '*.vhd')
+else:
+    # -- Used if a precompiled version is available
+    VU.add_external_library("unisim", args.unisim)
+
 lib = VU.add_library("EggNet", vhdl_standard="08")
 
-# -------------------------- 
+# --------------------------
 # -- Setup Libraries
 # --------------------------
 
@@ -106,37 +126,41 @@ lib.add_source_files(SRC_ROOT / "ShiftRegister" / "*.vhd")
 # lib.add_source_files(SRC_ROOT / "TopLevel" / "*.vhd")
 
 
-# -------------------------- 
-# -- Search for Testbenches
+# --------------------------
+# -- Search & Setup Testbenches
 # --------------------------
 
-# Find all testbenches ins path or search for a specific testbench
-if __name__ == "__main__":
+def add_tests(VU, ROOT, args):
+    # Find all testbenches ins path or search for a specific testbench
+
     TEST_PATH = pathlib.Path(args.testpath)
     assert TEST_PATH.is_dir(), "Testpath must be a directory"
+
     if args.testbench == "all":
         testbenches = sorted(TEST_PATH.rglob('tb_*.py'))
-        assert len(testbenches)>0,"No testbenches found in {}".format(TEST_PATH)
-    else:    
+        assert len(testbenches) > 0, "No testbenches found in {}".format(
+            TEST_PATH)
+    else:
         testbenches = sorted(TEST_PATH.rglob(args.testbench))
-        assert len(testbenches)>0,"Testbenches {}".format(args.testbench) + "not found in path {}".format(args.testpath)
-        
-import importlib.util
+        assert len(testbenches) > 0, "Testbenches {}".format(
+            args.testbench) + "not found in path {}".format(args.testpath)
 
-# -------------------------- 
-# -- Setup Testbenches
-# --------------------------
-def run_test(tests):
-    for testbench in tests:
-        spec = importlib.util.spec_from_file_location(testbench.stem,testbench)
+    for testbench in testbenches:
+        # -- Some magic applied here to dynamically important the modules
+        spec = importlib.util.spec_from_file_location(
+            testbench.stem, testbench)
         test_module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(test_module)
-        test = test_module.Testbench(VU,"EggNet",ROOT,vcd=args.vcd,synopsys=args.synopsys)
-        test.load_testdata()
-        test.execute()
 
+        # -- Add tests to VUNIT
+        test = test_module.Testbench(
+            VU, "EggNet", ROOT, vcd=args.vcd, synopsys=args.synopsys)
+        test.load_testdata()
 
 
 if __name__ == "__main__":
-    run_test(testbenches)
 
+    if args.compile_only == False:
+        add_tests(VU, ROOT, args)
+
+    VU.main()
--- vivado/NN_IP/EggNet_1.0/vunit/testbenches/simulator.py
+++ vivado/NN_IP/EggNet_1.0/vunit/testbenches/simulator.py
@@ -18,6 +18,7 @@ Dependencies:
 
 import os
 import pathlib
+import vunit
 from vunit import VUnit
 import random
 import numpy as np
@@ -28,49 +29,59 @@ import numpy2json2vhdl as np2vhdl
 
 
 class Simulator:
-    def __init__(self, vunit: VUnit, libname:str, root_path:pathlib.Path, child:pathlib.Path, testbench_name = None, vcd = False, synopsys = False):
+
+    @staticmethod
+    def append_source_files(lib: vunit.ui.Library):
+        raise NotImplementedError()
+
+    def __init__(self, vunit: VUnit, libname: str, root_path: pathlib.Path, child: pathlib.Path, testbench_name=None, vcd=False, synopsys=False):
         # -- Set up Vunit --
-        self.VU = vunit; # VUNIT class 
+        self.VU = vunit  # VUNIT class
         self.lib = vunit.library(libname)
-        
-        # -- Set up workspace -- 
+
+        # -- Set up workspace --
         self.LOCAL_ROOT = child.parent
         self.ROOT = root_path
         os.makedirs(self.ROOT / "tmp", exist_ok=True)
 
         if testbench_name == None:
             testbench_name = child.stem
-        print("---------------------------------------------------------------")    
-        print("VHDL Testbench: " + testbench_name + ".vhd")
+
         print("---------------------------------------------------------------")
+        print(" VHDL Testbench: " + testbench_name + ".vhd")
+        print("---------------------------------------------------------------")
+
         # -- Add vhdl testbench --
         self.lib.add_source_files(self.LOCAL_ROOT / (testbench_name + ".vhd"))
-        self.TB = self.lib.test_bench(testbench_name) # 
-        
+        self.TB = self.lib.test_bench(testbench_name)
+
         # -- Set compile options --
+
+        self.vcd_enabled = vcd
+        self.synopsys_enabled = synopsys
+
         if vcd == True:
-            self.TB.set_sim_option('ghdl.sim_flags', [f'--vcd={self.ROOT / "tmp" / (testbench_name + ".vcd")}'])
-        
-        if synopsys == True:    
+            self.TB.set_sim_option(
+                'ghdl.sim_flags', [f'--vcd={self.ROOT / "tmp" / (testbench_name + ".vcd")}'])
+
+        if synopsys == True:
             self.VU.set_compile_option("ghdl.flags", ["--ieee=synopsys"])
-        
-        
-    def load_testdata(self,np_array = None,MNIST_PATH:pathlib.Path = pathlib.Path(__file__).parents[5] / "data" / "MNIST", randseed = None):
+
+    def load_testdata(self, np_array=None, MNIST_PATH: pathlib.Path = pathlib.Path(__file__).parents[5] / "data" / "MNIST", randseed=None):
         # if no spezial numpy array is defined use random test image
         if np_array == None:
             mnist = EggNet.Reader.MNIST(folder_path=str(MNIST_PATH))
             test_images = mnist.test_images()
-            if randseed != None: # Use seeds for repeatability 
+
+            if randseed != None:  # Use seeds for repeatability
                 random.seed(randseed)
-            # Use random test image 
-            np_array = test_images[random.randint(0, np.shape(test_images)[0]-1)]
-            
-        np2vhdl.dump_json(np_array, self.Root / "tmp")
-        np2vhdl.setup_vunit(self.VU,self.TB, self.ROOT / "tmp")
-            
-    
+            # Use random test image
+            np_array = test_images[random.randint(
+                0, np.shape(test_images)[0]-1)]
+
+        np2vhdl.dump_json(np_array, self.ROOT / "tmp")
+        np2vhdl.setup_vunit(self.VU, self.TB, self.ROOT / "tmp")
+
     def execute(self):
         print("Execute simulation")
         self.VU.main()
-        # TODO: RUN simulation + print results 
-        
