6677a57676e1f3a01bf6552b907ffe5d5b899b6c
Lukas Baischer <e1426506@student.tuwien.ac.at>
2020-05-29 11:21:31 +0200

    update network simulation functions

--- python/EggNet/EggNet/VunitExtension/net_sim.py
+++ python/EggNet/EggNet/VunitExtension/net_sim.py
@@ -10,27 +10,23 @@ Network simulation functions
 Provides functions for creating test data for the use in vhdl testbenches
 Requirements: 
     numpy
-    random
-    pathlib
-    c
 """
 import numpy as np
-import random
 
 # %% memory controller
 
-def get_vectors_from_data(test_data, img_width, img_hight, kernel_size=3, dtype=np.uint8):
+def get_vectors_from_image(images, kernel_size=3, dtype=np.uint8):
     """
-    Generates 3x1 vectors from test data
+    Generates 3x1 vectors from image data
 
     Parameters
     ----------
-    test_data : numpy array
-        generated test data.
+    images : numpy array
+        [B,H,W,Ci].
     img_width : integer
         with of test matrix.
-    img_hight : integer
-        hight of test matrix.
+    img_height : integer
+        height of test matrix.
     kernel_size : integer, optional
         size of the kernel. The default is 3.
     dtype : numpy dtype, optional
@@ -38,36 +34,34 @@ def get_vectors_from_data(test_data, img_width, img_hight, kernel_size=3, dtype=
 
     Returns
     -------
-    vectors : numpy array
+    vectors : numpy array [B,H,W,K,Ci].
         Vector to compare with the output of the memory controller
 
     """
-    vectors = np.zeros((test_data.shape[0], test_data.shape[1], kernel_size, test_data.shape[2]), dtype=dtype)
-    for i in range(test_data.shape[0]):
-        vector_cnt = 0
-        for j in range(test_data.shape[1]):
-
-            if j < img_width:
-                vectors[i, vector_cnt, 0, :] = 0
-                vectors[i, vector_cnt, 1, :] = test_data[i, j, :]
-                vectors[i, vector_cnt, 2, :] = test_data[i, j + img_width, :]
-                vector_cnt += 1
-            elif j >= (img_width * (img_hight - 1)):
-                # print(j)
-                vectors[i, vector_cnt, 0, :] = test_data[i, j - img_width, :]
-                vectors[i, vector_cnt, 1, :] = test_data[i, j, :]
-                vectors[i, vector_cnt, 2, :] = 0
-                vector_cnt += 1
-            else:
-                vectors[i, vector_cnt, 0, :] = test_data[i, j - img_width, :]
-                vectors[i, vector_cnt, 1, :] = test_data[i, j, :]
-                vectors[i, vector_cnt, 2, :] = test_data[i, j + img_width, :]
-                vector_cnt += 1
+    img_width = images.shape[2]
+    img_height = images.shape[1]
+    vectors = np.zeros((images.shape[0], images.shape[1], images.shape[2], kernel_size,images.shape[3]), dtype=dtype)
+    for i in range(images.shape[0]): #iterate batches
+        for j in range(img_height):
+            for k in range(img_width):
+    
+                if j == 0: # Upper Padding
+                    vectors[i, j, k, 0,:] = 0
+                    vectors[i, j, k, 1,:] = images[i, j, k,:]
+                    vectors[i, j, k, 2,:] = images[i, j+1, k,:]
+                elif j >= (img_height - 1): # Lower Padding
+                    vectors[i, j, k, 0,:] = images[i, j-1, k,:]
+                    vectors[i, j, k, 1,:] = images[i, j, k,:]
+                    vectors[i, j, k, 2,:] = 0
+                else:
+                    vectors[i, j, k, 0,:] = images[i, j-1, k,:]
+                    vectors[i, j, k, 1,:] = images[i, j, k,:]
+                    vectors[i, j, k, 2,:] = images[i, j+1, k,:]
 
     return vectors
 
 #%% 3x3 shiftregister
-def get_Kernels(test_vectors, img_width):
+def get_Kernels(test_vectors):
     """
     Creates 3x3 kernel which is operated by the conv2d
 
@@ -79,27 +73,28 @@ def get_Kernels(test_vectors, img_width):
         with of test matrix.
     Returns
     -------
-    Kernel : numpy array
+    Kernel : numpy array [B,H,W,Kh,Kw, Ci]
         Kernel to compare with the output of the shiftregister
 
     """
-    kernels = np.zeros((test_vectors.shape[0], test_vectors.shape[1], test_vectors.shape[2], test_vectors.shape[2],
-                        test_vectors.shape[3]), dtype=np.uint8)
+    kernels = np.zeros((test_vectors.shape[0], test_vectors.shape[1], test_vectors.shape[2], test_vectors.shape[3],
+                        test_vectors.shape[3], test_vectors.shape[4]), dtype=np.uint8)
     for i in range(test_vectors.shape[0]):
         for j in range(test_vectors.shape[1]):
-            if j % img_width == 0:
-                kernels[i, j, :, 0, :] = 0
-                kernels[i, j, :, 1, :] = test_vectors[i, j, :, :]
-                kernels[i, j, :, 2, :] = test_vectors[i, j + 1, :, :]
-
-            elif j % img_width == img_width - 1:
-                kernels[i, j, :, 0, :] = test_vectors[i, j - 1, :, :]
-                kernels[i, j, :, 1, :] = test_vectors[i, j, :, :]
-                kernels[i, j, :, 2, :] = 0
-            else:
-                kernels[i, j, :, 0, :] = test_vectors[i, j - 1, :, :]
-                kernels[i, j, :, 1, :] = test_vectors[i, j, :, :]
-                kernels[i, j, :, 2, :] = test_vectors[i, j + 1, :, :]
+            for k in range(test_vectors.shape[2]):
+                if k == 0: # Left padding
+                    kernels[i, j, k, :, 0,:] = 0
+                    kernels[i, j, k, :, 1,:] = test_vectors[i, j, k, :, :]
+                    kernels[i, j, k, :, 2,:] = test_vectors[i, j, k+1, :, :]
+    
+                elif k == test_vectors.shape[2] - 1: # Right padding
+                    kernels[i, j, k, :, 0, :] = test_vectors[i, j, k-1, :, :]
+                    kernels[i, j, k, :, 1, :] = test_vectors[i, j, k, :, :]
+                    kernels[i, j, k, :, 2, :] = 0
+                else:
+                    kernels[i, j, k, :, 0, :] = test_vectors[i, j, k-1, :, :]
+                    kernels[i, j, k, :, 1, :] = test_vectors[i, j, k, :, :]
+                    kernels[i, j, k, :, 2, :] = test_vectors[i, j, k+1, :, :]
 
     return kernels
 
@@ -112,17 +107,18 @@ def conv_2d(kernels, weights, msb):
 
     Parameters
     ----------
-    kernel : numpy array [B,W*H,Kh,Kw,Ci]
+    kernel : numpy array [B,H,W,Kh,Kw,Ci]
         B.. Batch size
-        W*H.. Image width times hight
-        Kh.. Kernel hight
+        H.. Image height
+        W.. Image width 
+        Kh.. Kernel height
         Kw.. Kernel width
         Ci.. channel number
         Input kernels
     weights : numpy array [Co,Ci,Kh,Kw]
         Co.. output channel number
         Ci.. input channel number
-        Kh.. Kernel hight
+        Kh.. Kernel height
         Kw .. Kernel with
         Weigth matrix for each kernel
     msb : numpy array [Co,Ci]
@@ -131,136 +127,20 @@ def conv_2d(kernels, weights, msb):
 
     Returns
     -------
-    features: numpy array [B,W*H,Co] dtype=np.uint8
-        B.. Batch size
-        W*H.. Image width times hight
-        Co.. output channel number
-
-        8 bit output Matrix
-    """
-    features = np.zeros((kernels.shape[0], kernels.shape[1], weights.shape[0]), dtype=np.uint8)
-    for i in range(kernels.shape[0]):
-        for j in range(kernels.shape[1]):
-            for k in range(weights.shape[0]):
-                features[i, j, k] = conv_channel(kernels[i, j, :, :, :], weights[k, :, :, :], msb[k])
-    return features
-
-
-def conv_channel(kernels, weights, msb):
-    """
-    Emulates the operation carried out by the conv_channel module in the FPGA
-
-    Parameters
-    ----------
-    kernels : numpy array [B,W*H,Kh,Kw,Ci]
-        B.. Batch size
-        W*H.. Image width times hight
-        Kh.. Kernel hight
-        Kw.. Kernel width
-        Ci.. channel number
-        Input kernels
-    weights : numpy array [Ci,Kh,Kw]
-        Ci.. input channel number
-        Kh.. Kernel hight
-        Kw .. Kernel with
-        Weigth matrix for each kernel
-    msb : integer
-        MSB postion for quantization
-
-    Returns
-    -------
-    weighted_sum: np.uint8
-        B.. Batch size
-        W*H.. Image width times hight
-
-        8 bit output Matrix
-    """
-    weighted_sum = np.int32(0)
-    for k in range(weights.shape[0]):
-        weighted_sum += kernel_3x3(kernels[:, :, k], weights[k, :, :])
-
-    # Relu (Additional benefit np.int16(int("0x00FF",16)) & feature would not work for negative numbers because of 2's complement)
-    if weighted_sum < 0:
-        weighted_sum = 0
-    else:  # Quantization
-        weighted_sum >>= msb + 1 - 8
-        if weighted_sum > 255:
-            weighted_sum = 255
-
-    return np.uint8(weighted_sum)
-
-
-def kernel_3x3(kernel, weights):
-    """
-    Emulates the operation carried out by the 3x3_kernel module in the FPGA
-
-    Parameters
-    ----------
-    kernel : numpy array [Kh,Kw]
-        Kh.. Kernel hight
-        Kw.. Kernel width
-        Input kernels
-    weights : numpy array [Kh,Kw]
-        Kh.. Kernel hight
-        Kw .. Kernel with
-        Weigth matrix for each kernel
-
-    Returns
-    -------
-    weighted_sum: np.int16
-        16 bit output Matrix
-    """
-    weighted_sum = np.int32(np.sum(kernel * weights))
-    return weighted_sum
-
-# %% log 2 convolutional layer
-
-def conv_2d_log2(kernels, weight_shifts, weight_signs, fraction_shift_channel, faction_shift_kernel):
-    """
-    Emulates the operation carried out by the conv2d module in the FPGA
-
-    Parameters
-    ----------
-    kernel : numpy array [B,H,W,Kh,Kw,Ci]
-        B.. Batch size
-        H.. Image hight
-        W.. Image width
-        Kh.. Kernel hight
-        Kw.. Kernel width
-        Ci.. channel number
-        Input kernels
-    weight_shifts : numpy array [Co,Ci,Kh,Kw]
-        Co.. output channel number
-        Ci.. input channel number
-        Kh.. Kernel hight
-        Kw .. Kernel with
-        Weigth shift matrix for each kernel
-    weight_signs : numpy array [Co,Ci,Kh,Kw]
-        Co.. output channel number
-        Ci.. input channel number
-        Kh.. Kernel hight
-        Kw .. Kernel with
-        Signs of shift weights matrix for each kernel        
-    fraction_shift_channel: integer
-        fraction shifts for quantization after conv_channels
-    faction_shift_kernel: integer
-        fraction shifts for quantization after each 3x3 kernel
-        
-    Returns
-    -------
     features: numpy array [B,H,W,Co] dtype=np.uint8
         B.. Batch size
-        H.. Image hight
-        W.. Image width
+        H.. Image height
+        W.. Image width 
         Co.. output channel number
 
         8 bit output Matrix
     """
-    features = np.zeros((kernels.shape[0], kernels.shape[1], weights.shape[0]), dtype=np.uint8)
+    features = np.zeros((kernels.shape[0], kernels.shape[1], kernels.shape[2], weights.shape[0]), dtype=np.uint8)
     for i in range(kernels.shape[0]):
         for j in range(kernels.shape[1]):
-            for k in range(weights.shape[0]):
-                features[i, j, k] = conv_channel(kernels[i, j, :, :, :], weights[k, :, :, :], msb[k])
+            for k in range(kernels.shape[1]):
+                for l in range(weights.shape[0]):
+                    features[i, j, k, l] = conv_channel(kernels[i, j, k, :, :, :], weights[l, :, :, :], msb[l])
     return features
 
 
@@ -272,14 +152,14 @@ def conv_channel(kernels, weights, msb):
     ----------
     kernels : numpy array [B,W*H,Kh,Kw,Ci]
         B.. Batch size
-        W*H.. Image width times hight
-        Kh.. Kernel hight
+        W*H.. Image width times height
+        Kh.. Kernel height
         Kw.. Kernel width
         Ci.. channel number
         Input kernels
     weights : numpy array [Ci,Kh,Kw]
         Ci.. input channel number
-        Kh.. Kernel hight
+        Kh.. Kernel height
         Kw .. Kernel with
         Weigth matrix for each kernel
     msb : integer
@@ -289,7 +169,7 @@ def conv_channel(kernels, weights, msb):
     -------
     weighted_sum: np.uint8
         B.. Batch size
-        W*H.. Image width times hight
+        W*H.. Image width times height
 
         8 bit output Matrix
     """
@@ -315,11 +195,11 @@ def kernel_3x3(kernel, weights):
     Parameters
     ----------
     kernel : numpy array [Kh,Kw]
-        Kh.. Kernel hight
+        Kh.. Kernel height
         Kw.. Kernel width
         Input kernels
     weights : numpy array [Kh,Kw]
-        Kh.. Kernel hight
+        Kh.. Kernel height
         Kw .. Kernel with
         Weigth matrix for each kernel
 
@@ -330,6 +210,7 @@ def kernel_3x3(kernel, weights):
     """
     weighted_sum = np.int32(np.sum(kernel * weights))
     return weighted_sum
+
 # %% Result checks
 def check_bram(test_data, layernumber):
     """
