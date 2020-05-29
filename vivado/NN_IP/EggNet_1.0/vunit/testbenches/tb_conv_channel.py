a4773f5daa6b82224816f062a597211dc8a857ad
Lukas Baischer <e1426506@student.tuwien.ac.at>
2020-05-29 11:31:45 +0200

    add first exampe for reading 5d numpy array using vhdl

--- vivado/NN_IP/EggNet_1.0/vunit/testbenches/tb_conv_channel.py
+++ vivado/NN_IP/EggNet_1.0/vunit/testbenches/tb_conv_channel.py
@@ -19,6 +19,7 @@ Inherits:
 
 import pathlib
 from vunit import VUnit
+import numpy as np
 
 import EggNet.VunitExtension as EggUnit
 
@@ -30,7 +31,15 @@ class Testbench(EggUnit.Simulator):
        super().__init__(vunit, libname, root_path,pathlib.Path(__file__),testbench_name=testbench_name,vcd=vcd,synopsys=synopsys)
         
     def load_testdata(self):
-        super().load_testdata()       
+        images = super()._use_rand_images(3,randseed=1)
+        images_c = np.zeros(images.shape + (1,)) 
+        images_c[:,:,:,0] = images
+        vectors = EggUnit.get_vectors_from_image(images_c)
+        kernels = EggUnit.get_Kernels(vectors)
+        #test = np.arange(45,dtype=np.uint8)
+        #test = test.reshape([5,3,3])
+        #super().load_testdata(test,"testdata.csv") 
+        super().load_testdata(kernels[:,:,:,:,:,0],"testdata.csv")       
     
     def execute(self):
         super().execute()
@@ -41,9 +50,13 @@ if __name__ == "__main__":
     # -- Import run.py 
     ROOT = pathlib.Path(__file__).parents[1]
     RUN_PATH = ROOT / "run.py"
+    SRC_ROOT = ROOT.parent / 'src'
     spec = importlib.util.spec_from_file_location(RUN_PATH.stem,RUN_PATH)
-    simulation = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(simulation)
-    simulation.run_test([pathlib.Path(__file__)])
+    run_py = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(run_py)
     
-    # -- use run.py with 
+    # -- Setup arguments 
+    args = ['-t', pathlib.Path(__file__).stem, '--testpath',str(pathlib.Path(__file__).parent.absolute())]
+    # -- Initialze vunit 
+    runner = run_py.VU_Run(ROOT,SRC_ROOT,args)
+    runner.run_test()
--- vivado/NN_IP/EggNet_1.0/vunit/testbenches/tb_conv_channel.vhd
+++ vivado/NN_IP/EggNet_1.0/vunit/testbenches/tb_conv_channel.vhd
@@ -1,59 +1,78 @@
 library vunit_lib;
 context vunit_lib.vunit_context;
-library JSON;
-context JSON.json_ctx;
 LIBRARY work;
-use work.json_numpy.all;
+use work.csv_numpy.all;
+use STD.textio.all;
 
-entity tb_json_gens is
+entity tb_conv_channel is
   generic (
     RUNNER_CFG : string;
     TB_PATH    : string;
-    TB_CFG     : string;
-    TB_CHANNEL_NUMBER : integer; 
+    TB_CSV_FILE     : string;
+    TB_BATCH_NUMBER : integer; 
+    TB_IMG_HEIGHT : integer; 
+    TB_IMG_WIDTH : integer; 
+    TB_KERNEL_HEIGHT : integer; 
+    TB_KERNEL_WIDTH : integer 
   );
 end entity;
 
-architecture tb of tb_json_gens is
+architecture tb of tb_conv_channel is
 
-signal kernel : int_vec_2d_t(,2 downto 0, 2 downto 0); 
+--signal image_data : jsonGetNumpy5d(TB_BATCH_NUMBER-1 downto 0, TB_IMG_HEIGHT-1 downto 0, TB_IMG_WIDTH-1 downto 0, TB_KERNEL_HEIGHT-1 downto 0, TB_KERNEL_WIDTH-1 downto 0);
 
 
 begin
   main: process
-
-    procedure run_example(JSONContent : T_JSON) is
+    procedure run_test(filepath : string) is
       -- get array of integers from JSON content
-      constant img_dim : integer_vector := jsonGetIntegerArray(JSONContent, "dim");
-      constant img_arr : int_vec_3d_t := jsonGetNumpy3d(JSONContent);
+      constant img_dim : integer_vector := csvGetNumpyDim(filepath);
+      constant img_arr : int_vec_5d_t := csvGetNumpy5d(filepath);
+      variable k_line : line;
     begin
-      -- Content extracted from the JSON
-      info("JSONContent: " & lf & JSONContent.Content);
 
       -- Print dimension integer array, extracted by function jsonGetIntegerArray(JSON) with data from the JSON
       for i in 0 to img_dim'length-1 loop
         info("Image dim [" & integer'image(i) & "]: " & integer'image(img_dim(i)));
       end loop;
       -- Print numpy integer array, extracted by function jsonGetNumpy3d(json_numpy) with data from the JSON
-      for k in 0 to img_dim(0)-1 loop 
-        for j in 0 to img_dim(1)-1 loop 
-          for i in 0 to img_dim(2)-1 loop
-            info("Array data [" & integer'image(k) & "][" & integer'image(j) & "][" & integer'image(i) & "]: "& integer'image(img_arr(k,j,i)));
-          end loop; 
+      info("Image");
+      -- for k in 0 to img_dim(0)-1 loop --batch
+        -- info("Batch[" & integer'image(k));
+        -- for j in 0 to img_dim(1)-1 loop --kernel height
+          -- write(k_line, string'("|"));
+          -- for i in 0 to img_dim(2)-1 loop --kernel width
+            -- --info(integer'image(img_arr(k,j,i)));
+            -- write(k_line, (string'(" ") & integer'image(img_arr(k,j,i))));
+          -- end loop; 
+          -- write(k_line, string'(" |"));
+          -- writeline(output,k_line);
+        -- end loop;
+      -- end loop;        
+      for m in 0 to img_dim(0)-1 loop --batch
+        for l in 0 to img_dim(1)-1 loop --height
+          for k in 0 to img_dim(2)-1 loop --widht
+            info("Batch[" & integer'image(m) & "] Height[" & integer'image(l) & "] Width[" & integer'image(k) & "]: ");
+            for j in 0 to img_dim(3)-1 loop --kernel height
+              write(k_line, string'("|"));
+              for i in 0 to img_dim(4)-1 loop --kernel width
+                write(k_line, (string'(" ") & integer'image(img_arr(m,l,k,j,i))));
+              end loop; 
+              write(k_line, string'(" |"));
+              writeline(output,k_line);
+            end loop;
+          end loop;        
         end loop;
       end loop;
     end procedure;
 
 
-    variable JSONContent : T_JSON;
-
   begin
     test_runner_setup(runner, RUNNER_CFG);
     while test_suite loop
-      info("RAW generic: " & TB_CFG);
-      if run("Conv channel test") then
-        JSONContent := jsonLoad(TB_CFG);
-        run_example(JSONContent);
+      info("CSV file path: " & TB_CSV_FILE);
+      if run("CSV test") then
+        run_test(TB_CSV_FILE);
       end if;
     end loop;
     test_runner_cleanup(runner);
