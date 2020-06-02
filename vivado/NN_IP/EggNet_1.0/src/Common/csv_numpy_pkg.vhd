d5c3670d409116845a115ec067a54a4ce478632f
Lukas Baischer <e1426506@student.tuwien.ac.at>
2020-06-02 11:48:18 +0200

    add print functions

--- vivado/NN_IP/EggNet_1.0/src/Common/csv_numpy_pkg.vhd
+++ vivado/NN_IP/EggNet_1.0/src/Common/csv_numpy_pkg.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 library vunit_lib;
 context vunit_lib.vunit_context;
+use STD.textio.all;
 
 -- Based on JSON-for-VHDL: https://github.com/Paebbels/JSON-for-VHDL.git
 
@@ -16,6 +17,8 @@ package csv_numpy is
   impure function csvGetNumpy4d(filepath : string) return int_vec_4d_t;
   impure function csvGetNumpy5d(filepath : string) return int_vec_5d_t;
   impure function csvGetNumpyDim(filepath : string) return integer_vector;
+  
+  procedure  print5d(arr: int_vec_5d_t); 
   
 end package csv_numpy;
 
@@ -142,4 +145,71 @@ package body csv_numpy is
     info("Dimensions: " & integer'image(get(arr, 0)));
     return csvGetNumpy5d_data(arr,dim);
   end;
+
+  procedure  print2d(arr: int_vec_2d_t) is 
+    variable k_line : line;
+  begin
+    for j in 0 to arr'length(1)-1 loop --height
+      write(k_line, string'("|"));
+      for i in 0 to arr'length(2)-1 loop --width
+        write(k_line, (string'(" ") & integer'image(arr(j,i))));
+      end loop; 
+      write(k_line, string'(" |"));
+      writeline(output,k_line);
+    end loop;
+  end; 
+
+  procedure  print3d(arr: int_vec_3d_t) is 
+    variable k_line : line;
+  begin
+    for k in 0 to arr'length(1)-1 loop --depth
+      info("Depth[" & integer'image(k));
+      for j in 0 to arr'length(2)-1 loop --height
+        write(k_line, string'("|"));
+        for i in 0 to arr'length(3)-1 loop --width
+          write(k_line, (string'(" ") & integer'image(arr(k,j,i))));
+        end loop; 
+        write(k_line, string'(" |"));
+        writeline(output,k_line);
+      end loop;
+    end loop;   
+  end; 
+
+  procedure  print4d(arr: int_vec_4d_t) is 
+    variable k_line : line;
+  begin
+    for l in 0 to arr'length(1)-1 loop --batch
+      for k in 0 to arr'length(2)-1 loop --depth
+        info("Batch[" & integer'image(l) & "] Depth[" & integer'image(k) & "]: ");
+        for j in 0 to arr'length(3)-1 loop --kernel height
+          write(k_line, string'("|"));
+          for i in 0 to arr'length(4)-1 loop --kernel width
+            write(k_line, (string'(" ") & integer'image(arr(l,k,j,i))));
+          end loop; 
+          write(k_line, string'(" |"));
+          writeline(output,k_line);
+        end loop;     
+      end loop;
+    end loop;
+  end; 
+  
+  procedure  print5d(arr: int_vec_5d_t) is 
+    variable k_line : line;
+  begin
+    for m in 0 to arr'length(1)-1 loop --batch
+      for l in 0 to arr'length(2)-1 loop --height
+        for k in 0 to arr'length(3)-1 loop --widht
+          info("Batch[" & integer'image(m) & "] Height[" & integer'image(l) & "] Width[" & integer'image(k) & "]: ");
+          for j in 0 to arr'length(4)-1 loop --kernel height
+            write(k_line, string'("|"));
+            for i in 0 to arr'length(5)-1 loop --kernel width
+              write(k_line, (string'(" ") & integer'image(arr(m,l,k,j,i))));
+            end loop; 
+            write(k_line, string'(" |"));
+            writeline(output,k_line);
+          end loop;
+        end loop;        
+      end loop;
+    end loop;
+  end; 
 end csv_numpy;
